92800d0eaf769da6e386e8a5a893bb0e4bf736cc
GoranZec3 <goranzec1988@gmail.com>
2023-02-20 20:08:06 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+         String myLocation = "data\\";
+
+         ShiftAndRename shiftAndRename = new ShiftAndRename(myLocation, "frame", 60);
+
+         shiftAndRename.doShiftAndRename();
+
+    }
+}
